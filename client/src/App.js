b27c51a87b00b3b267094e7c9077763580a6c1d4
Dilan Marcano <d.marcano@DAKA.LOCAL>
2023-03-07 14:35:47 -0400

    git commit -m

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,48 @@
 import "./styles/App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  const fetchData = async () => {
-    try {
-      const res = await fetch(`http://localhost:5000/api/v1/`);
-      const data = await res.json();
-      console.log([...data]);
-      setUser([...data]);
-    } catch (error) {
-      console.error(error);
-    }
+  const onCaptureName = (event) => {
+    setName(event.target.value);
+  };
+  const onCaptureLastName = (event) => {
+    setLastName(event.target.value);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const sendData = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: name, lastname: lastName }),
+    };
+
+    const response = await fetch(
+      `http://localhost:5000/api/v1/newUser`,
+      requestOptions
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>view client from infocent</h1>
-      <table>
-        <tr>
-          <th>id</th>
-          <th>name</th>
-          <th>name</th>
-          <th>creteAt</th>
-        </tr>
-        {user.map((value) => (
-          <tr>
-            <td>{value.id}</td>
-            <td>{value.name}</td>
-            <td>{value.lastname}</td>
-            <td>{value.creteAt}</td>
-          </tr>
-        ))}
-      </table>
+      <p>view client from infocent</p>
+      <input
+        className="input"
+        placeholder="type a value"
+        onChange={onCaptureName}
+      />
+      <input
+        className="input"
+        placeholder="type a value"
+        onChange={onCaptureLastName}
+      />
+      <button
+      onClick={sendData}
+      >send request</button>
     </div>
   );
 }
