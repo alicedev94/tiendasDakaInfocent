54288fc8b3ed10621113518ee35373fc31817de5
Dilan Marcano <d.marcano@DAKA.LOCAL>
2023-03-07 10:17:15 -0400

    git commit -m

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`http://localhost:5000/api/v1/`);
+      const data = await res.json();
+      console.log([...data]);
+      setUser([...data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>view client from infocent</h1>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>name</th>
+          <th>creteAt</th>
+        </tr>
+        {user.map((value) => (
+          <tr>
+            <td>{value.id}</td>
+            <td>{value.name}</td>
+            <td>{value.lastname}</td>
+            <td>{value.creteAt}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = require("./routes/main");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1", router);
 
 app.listen(5000, () => {
